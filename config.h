af66e1d30ea361d6041a179851ce5729cb8d55b0
Andreas Malmqvist <andreasmalmqvist1992@gmail.com>
2019-03-11 12:44:45 +0100

    udpates

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hasklig:style=Bold:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Hasklig:style=Bold:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 30;
 
 /*
